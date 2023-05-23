146e80d001a5f5f6f41256c1f9115c0fff6a0ecc
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-05-23 23:07:35 +0530

    init

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const toggleIcon = document.querySelector('.toggle-icon');
 
 toggleIcon.addEventListener('click', () => {
     toggleIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
 });
